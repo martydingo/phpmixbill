56fd4cc665ac39fffcbf2d00d10a6a2ecf7d4069
martydingo <49862656+martydingo@users.noreply.github.com>
2020-01-16 12:23:53 +0000

    Delete config.old.php

--- system/config.old.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$db_host	    = '127.0.0.1';
-	$db_user        = 'admin';
-	$db_password	= 'password';
-	$db_name	    = 'phpmixbill';
-	define('APP_URL', 'http://seedbox.dingopaws.net:51338/phpmixbill');
-	$_app_stage = 'Live';
